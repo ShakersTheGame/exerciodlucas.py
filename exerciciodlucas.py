24edc31fcd42ec13a490d53d7d50c011f8ecdd02
1d-ogo1 <116488327+1d-ogo1@users.noreply.github.com>
2023-11-14 21:43:17 +0000

    Update exerciciodlucas.py

--- exerciciodlucas.py
+++ exerciciodlucas.py
@@ -1,17 +1,17 @@
 palavra1 = input().upper()
 palavra2 = input().upper()
-a = 0
+a = True
 
 
 for palavra in palavra1:
     if palavra.isalpha() is True:
         if palavra1.count(palavra) != palavra2.count(palavra):
-            a = 1
+            a = False
             break
         else:
             a = a
 
-if a == True:
+if a is True:
     print("yes")
 else:
     print("no")
